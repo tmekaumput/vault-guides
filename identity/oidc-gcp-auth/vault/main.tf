f2b117f082ca25d9eeb9a5c8029cc8e3cd2479b1
Top Mekaumput <top@hashicorp.com>
2023-02-14 14:05:12 +1100

    Adding groups claim

--- identity/oidc-gcp-auth/vault/main.tf
+++ identity/oidc-gcp-auth/vault/main.tf
@@ -31,15 +31,14 @@ resource "vault_jwt_auth_backend" "oidc" {
     oidc_discovery_url  = "https://accounts.google.com"
     oidc_client_id      = var.GCP_CLIENT_ID
     oidc_client_secret  = var.GCP_CLIENT_SECRET
-    # bound_issuer        = "https://myco.auth0.com/"
+
     provider_config = {
         provider = "gsuite"
-        gsuite_service_account = var.GCP_CREDENTIAL_FILE
+        gsuite_service_account = file(var.GCP_CREDENTIAL_FILE)
         gsuite_admin_impersonate = var.GCP_ADMIN_IMPERSONATE_EMAIL
-        fetch_groups = false
-        fetch_user_info = false
+        fetch_groups = true
+        fetch_user_info = true
         groups_recurse_max_depth = 5
-        user_custom_schemas = "Education,Preferences"
     }
 }
 
@@ -50,5 +49,6 @@ resource "vault_jwt_auth_backend_role" "oidc" {
   user_claim="sub"
   groups_claim="groups"
   role_type             = "oidc"
+  verbose_oidc_logging = true
   allowed_redirect_uris = ["http://localhost:8200/ui/vault/auth/oidc/oidc/callback"]
 }
